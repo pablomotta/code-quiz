8973d6ed9bed067c11a65b1b548c0719deb34eff
Pablo <pmotta.dev@gmail.com>
2019-09-26 19:14:27 -0700

    refactor logic - loaded questions

--- js/index.js
+++ js/index.js
@@ -1,160 +1,118 @@
 // quiz api
 var quizData = [
     {
-        id: 0,
         question: 'Inside which HTML element do we put the JavaScript?',
-        answers: ['<js>', '<scripting>', '<script>', '<javascript>'],
-        correct: '<script>'
+        choice1: '<js>',
+        choice2: '<scripting>',
+        choice3: '<script>',
+        choice4: '<javascript>',
+        correct: 3
     },
     {
-        id: 1,
         question: 'What are variables used for in JavaScript Programs?',
-        answers: [
-            'Storing numbers, dates, or other values',
-            ' Varying randomly',
-            'Causing high-school algebra flashbacks',
-            'None of the above'
-        ],
-        correct: 'Storing numbers, dates, or other values'
+        choice1: 'Storing numbers, dates, or other values',
+        choice2: ' Varying randomly',
+        choice3: 'Causing high-school algebra flashbacks',
+        choice4: 'None of the above',
+        correct: 1
     },
     {
-        id: 2,
         question:
             "Which of the following can't be done with client-side JavaScript?",
-        answers: [
-            'Validating a form',
-            "Sending a form's contents by email",
-            "Storing the form's contents to a database file on the server",
-            'None of the above'
-        ],
-        correct: "Storing the form's contents to a database file on the server"
+        choice1: 'Validating a form',
+        choice2: "Sending a form's contents by email",
+        choice3: "Storing the form's contents to a database file on the server",
+        choice4: 'None of the above',
+        correct: 3
     },
     {
-        id: 3,
         question:
             'Which of the following are capabilities of functions in JavaScript?',
-        answers: [
-            'Return a value',
-            'Accept parameters and Return a value',
-            'Accept parameters',
-            'None of the above'
-        ],
-        correct: 'Accept parameters'
+        choice1: 'Return a value',
+        choice2: 'Accept parameters and Return a value',
+        choice3: 'Accept parameters',
+        choice4: 'None of the above',
+        correct: 3
     },
     {
-        id: 4,
         question:
             'What is the correct JavaScript syntax to write "Hello World"?',
-        answers: [
-            'System.out.println("Hello World")',
-            'println ("Hello World")',
-            'document.write("Hello World")',
-            'response.write("Hello World")'
-        ],
-        correct: 'document.write("Hello World")'
+
+        choice1: 'System.out.println("Hello World")',
+        choice2: 'println ("Hello World")',
+        choice3: 'document.write("Hello World")',
+        choice4: 'response.write("Hello World")',
+        correct: 3
     },
     {
-        id: 5,
         question: 'What does the <noscript> tag do?',
-        answers: [
-            'Enclose text to be displayed by non-JavaScript browsers.',
-            'Prevents scripts on the page from executing.',
-            'Describes certain low-budget movies.',
-            'None of the above'
-        ],
-        correct: 'Enclose text to be displayed by non-JavaScript browsers.'
+        choice1: 'Enclose text to be displayed by non-JavaScript browsers.',
+        choice2: 'Prevents scripts on the page from executing.',
+        choice3: 'Describes certain low-budget movies.',
+        choice4: 'None of the above',
+        correct: 1
     },
     {
-        id: 6,
         question: 'Which of the following best describes JavaScript?',
-        answers: [
-            'a low-level programming language.',
-            'a scripting language pre-compiled in the browser.',
-            'a compiled scripting language.',
-            'an object-oriented scripting language.'
-        ],
-        correct: 'an object-oriented scripting language.'
+        choice1: 'a low-level programming language.',
+        choice2: 'a scripting language pre-compiled in the browser.',
+        choice3: 'a compiled scripting language.',
+        choice4: 'an object-oriented scripting language.',
+        correct: 4
     },
     {
-        id: 7,
         question: 'JavaScript is interpreted by _________',
-        answers: ['Client', 'Server', 'Object', ' None of the above'],
-        correct: 'Client'
+        choice1: 'Client',
+        choice2: 'Server',
+        choice3: 'Object',
+        choice4: ' None of the above',
+        correct: 1
     },
     {
-        id: 8,
         question: 'Inside which HTML element do we put the JavaScript?',
-        answers: ['<js>', '<scripting>', '<script>', '<javascript>'],
-        correct: '<script>'
+        choice1: '<js>',
+        choice2: '<scripting>',
+        choice3: '<script>',
+        choice4: '<javascript>',
+        correct: 3
     },
     {
-        id: 9,
         question: 'Inside which HTML element do we put the JavaScript?',
-        answers: ['<js>', '<scripting>', '<script>', '<javascript>'],
-        correct: '<script>'
+        choice1: '<js>',
+        choice2: '<scripting>',
+        choice3: '<script>',
+        choice4: '<javascript>',
+        correct: 3
     }
 ];
-// score
-var questionQuantity = quizData.length;
-var rightAnswer = 0;
+
+var currentQuestion = {};
+var takingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
 
 // selectors
+var question = document.querySelector('#question');
 var homePage = document.querySelector('#home');
-var startQuiz = document.querySelector('#start-quiz');
+var startQuizBtn = document.querySelector('#start-quiz');
 var quizApp = document.querySelector('#quiz-app');
-var quizQuestion = document.querySelector('#question');
-var answerButtonsDiv = document.querySelector('#answer-buttons');
 
+function startQuiz() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...quizData];
+    loadNewQuestion();
+}
+function loadNewQuestion() {
+    questionCounter++;
+    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+}
 // start quiz
-startQuiz.addEventListener('click', function() {
+startQuizBtn.addEventListener('click', function() {
     quizApp.style.display = 'block';
     homePage.style.display = 'none';
-    renderQuestion();
+    startQuiz();
 });
-
-// render question
-var iterator = 0;
-function renderQuestion() {
-    var question = quizData[iterator].question;
-
-    var answerOptions = quizData[iterator].answers;
-
-    quizQuestion.innerText = question;
-
-    for (var i = 0; i < answerOptions.length; i++) {
-        var questionNumber = i + 1;
-
-        var button = document.createElement('button');
-        button.textContent = `Option ${questionNumber}:    ${answerOptions[i]} `;
-        // console.log(button);
-        answerButtonsDiv.appendChild(button);
-    }
-}
-
-// remove questions
-function removeQuestion() {}
-
-//check for right answer
-answerButtonsDiv.addEventListener('click', function() {
-    var correctAnswer = quizData[iterator].correct;
-    // console.log(correctAnswer);
-    var elementClicked = event.target;
-    var liString = elementClicked.parentElement.innerText;
-    var regex = new RegExp(correctAnswer, 'gi');
-    // console.log(liString);
-    if (liString.match(regex)) {
-        console.log('it works');
-        rightAnswer += 1;
-        iterator += 1;
-        // renderQuestion();
-
-        console.log(rightAnswer);
-    } else {
-        console.log('wrong answer');
-        iterator += 1;
-        // renderQuestion();
-    }
-});
-
-// var deleteQuestion = document.getElementById('answer-options');
-// console.dir(deleteQuestion.childNodes);
